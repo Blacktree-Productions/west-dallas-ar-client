fe8b43af03b3f128110dacd4b02a7929d57aa335
jonathancote84 <jonathancote92@gmail.com>
2020-08-19 09:40:02 -0500

    swipable-views

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,8 @@
 import React from "react";
 // import styled from "styled-components";
-// import Tabs from '@material-ui/core/Tabs';
-// import Tab from '@material-ui/core/Tab';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Select from '@material-ui/core/Select';
-// import SwipableViews from 'react-swipeable-views';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import SwipableViews from 'react-swipeable-views';
 import { MapboxGLMap } from ".";
 
 // const Header = styled("header")`
@@ -16,18 +14,61 @@ import { MapboxGLMap } from ".";
 //   justify-content: center;
 //   align-items: center;
 // `;
-
-const Layout = () => {
-  return (
-    <>
-      {/* <Header>
-        <h1>*.*</h1>
-      </Header> */}
-      <main>
-        <MapboxGLMap/> 
-      </main>
-    </>
-  );
+const styles = {
+  tabs: {
+    background: '#fff',
+  },
+  slide: {
+    padding: 15,
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    // backgroundColor: '#6AC0FF',
+  },
 };
+
+class Layout extends React.Component {
+  state = {
+    index: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({
+      index: value,
+    });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({
+      index,
+    });
+  };
+
+  render() {
+    const { index } = this.state;
+
+    return (
+      <div>
+        {/* a swipable views version with tabs, probably not using this as its no longer being developed with hooks */}
+        <Tabs value={index} fullWidth onChange={this.handleChange} style={styles.tabs}>
+          <Tab label="tab n°1"/>
+          <Tab label="tab n°2"/>
+          <Tab label="tab n°3"/>
+        </Tabs>
+        <SwipableViews index={index} onChangeIndex={this.handleChangeIndex}>
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>opening</div>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>commencement</div>
+          <MapboxGLMap/>
+        </SwipableViews>
+      </div>
+    )
+  }
+}
 
 export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Layout } from "./components";
+import Layout from "./components/Layout";
 
 const App = () => {
   return <Layout />;
